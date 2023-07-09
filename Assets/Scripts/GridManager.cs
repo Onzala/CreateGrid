76a4752b254ea117dc420d7042a2c17d6c071739
Onzala <32813383+Onzala@users.noreply.github.com>
2023-07-09 22:47:49 +0800

    更新0708
    調整顏色的Alpha值，結果可以顯示基礎板塊與Offset板塊的顏色差異了。

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -2,28 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GridManager : MonoBehaviour {
-    [SerializeField] private int _width, _height;
- 
-    [SerializeField] private Tile _tilePrefab;
- 
-    [SerializeField] private Transform _cam;
+public class GridManager : MonoBehaviour
+{
+    [SerializeField] private int _width, _height; //宣告長、寬變數
 
-    void GenerateGrid() {
-        
-        for (int x = 0; x < _width; x++) {
-            for (int y = 0; y < _height; y++) {
-                var spawnedTile = Instantiate(_tilePrefab, new Vector3(x, y), Quaternion.identity);
-                spawnedTile.name = $"Tile {x} {y}";
- 
-                var isOffset = (x % 2 == 0 && y % 2 != 0) || (x % 2 != 0 && y % 2 == 0);
+    [SerializeField] private Tile _tilePrefab; //建立預置物件(Prefab)參數
+
+    [SerializeField] private Transform _cam; //建立參數移動鏡頭
+    
+    void Start() {  //在開始時執行
+        GenerateGrid(); //這串的用途應該是執行GenerateGrid()這個功能
+    }
+    void GenerateGrid() { //生成板塊的功能
+        for (int x = 0; x<_width; x++) {  //迴圈：宣告X=0，如果X<_width,則增加X的數值(但不知為何是增加整數)
+            for (int y = 0; y<_height; y++) {  //迴圈：宣告Y=0，如果Y<_height,則增加y的數值(但不知為何是增加整數)
+                var spawnedTile = Instantiate(_tilePrefab,new Vector3(x,y),Quaternion.identity); //只知道這一段式生成板塊，但不知程式語言的用途
+                spawnedTile.name = $"Tile {x} {y}"; //生成板塊的名稱
+
+                var isOffset = (x % 2 == 0 && y % 2 != 0) || (x % 2 != 0 && y % 2 == 0); //
                 spawnedTile.Init(isOffset);
+            } 
         }
-      }
 
 
-    _cam.transform.position = new Vector3((float)_width/2 -0.5f, (float)_height/2 -0.5f, -10);
-
-   }
+        _cam.transform.position = new Vector3((float)_width/2 -0.5f,(float)_height/2 -0.5f,-10 );
+    }
 }
-
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
-    [SerializeField] private Color _basecolor,_offsetcolor;
-    [SerializeField] private SpriteRenderer _renderer;
+    [SerializeField] private Color _basecolor, _offsetcolor; //設置顏色變數
+    [SerializeField] private SpriteRenderer _renderer; //設置渲染器變數
+    [SerializeField] private GameObject _highlight; //設置遊戲物件變數
 
-    public void Init(bool isOffset) {
+    public void Init(bool isOffset) { 
         _renderer.color = isOffset ? _offsetcolor : _basecolor;
     }
 
+    void OnMouseEnter() {
+        _highlight.SetActive(true);
+    }
+    void OnMouseExit() {
+        _highlight.SetActive(false);
+    }
 }
